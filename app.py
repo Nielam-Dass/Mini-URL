0bcd0a4fd59ea73fc6676901fb5e67bf1ebe93f9
Nielam Dass <nielam.dass@gmail.com>
2025-01-18 02:26:34 -0500

    Added app home page template

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from dotenv import load_dotenv
 import os
 
@@ -9,7 +9,7 @@ SERVER_PORT = os.getenv('SERVER_PORT')
 
 @app.route('/')
 def hello_world():
-    return "Flask app running"
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True, port=SERVER_PORT)
