98a340d139aa8f703741a9e050fd16f4e88a451e
Nielam Dass <nielam.dass@gmail.com>
2025-01-18 20:51:50 -0500

    Added URL submission form

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from dotenv import load_dotenv
 import os
 
@@ -10,6 +10,11 @@ SERVER_PORT = os.getenv('SERVER_PORT')
 @app.route('/')
 def hello_world():
     return render_template('index.html')
+
+@app.post('/minify-url')
+def minify_url():
+    original_url = request.form.get('original-url')
+    return f'Received url: {original_url}'
 
 if __name__ == '__main__':
     app.run(debug=True, port=SERVER_PORT)
