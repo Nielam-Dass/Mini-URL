46cbd2ee6063091ab886403c157e7479156c377e
Nielam Dass <nielam.dass@gmail.com>
2025-02-12 01:57:05 -0500

    Added remote Terraform Cloud backend to TF configuration

--- main.tf
+++ main.tf
@@ -5,6 +5,13 @@ terraform {
             version = "~> 5.84"
         }   
     }
+    cloud {
+        organization = "nielam-dass"
+        workspaces {
+            name = "mini-url-dev"
+            project = "Mini-URL-App"
+        }
+    }
 }
 
 provider "aws" {
