f46679db08e3ddef0726a53cfb9a8d601d640872
Nielam Dass <nielam.dass@gmail.com>
2025-03-13 23:54:21 -0400

    Store server port number in variable for readability

--- run.py
+++ run.py
@@ -5,4 +5,5 @@ from mini_url.app import app
 import os
 
 if __name__ == "__main__":
-    app.run(debug=True, port=os.getenv('SERVER_PORT', 5000))
+    SERVER_PORT = os.getenv('SERVER_PORT', 5000)
+    app.run(debug=True, port=SERVER_PORT)
