3a949fc009d56a403073888fd443b4177b29eaf1
Nielam Dass <nielam.dass@gmail.com>
2025-08-12 00:40:11 -0400

    Added extra assertions under test_index

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,4 +1,9 @@
 def test_index(client):
-    response = client.get('/')
-    assert b'<h1>MiniURL</h1>' in response.data
+    response = client.get("/")
+    assert response.status_code == 200
+    assert b"<h1>MiniURL</h1>" in response.data
+    assert b"<h2>Minify big URLs quickly...</h2>" in response.data
+    assert b"<input type=\"text\" name=\"original-url\" id=\"original-url\">" in response.data
+    assert b"<input type=\"submit\" value=\"Submit\">" in response.data
+    assert b"<p>Your MiniURL:</p>" not in response.data
     
